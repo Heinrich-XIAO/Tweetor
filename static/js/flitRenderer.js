e0a9edfb6482aa08edc28d1996b2a44cac3bf545
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2023-12-14 01:18:36 +0000

    Online/offline icon

--- static/js/flitRenderer.js
+++ static/js/flitRenderer.js
@@ -15,121 +15,14 @@ function getMonthAbbreviation(date) {
 
 async function renderFlits() {
   const res = await fetch(`/api/get_flits?skip=${skip}&limit=${limit}`); //////////////////////////////// possible http param inject
-  console.log(await res.clone().text())
   const json = await res.json();
-  console.log(json)
   for (let flitJSON of json) {
-    const flit = document.createElement('div');
-    flit.classList.add('flit');
-    const flit_data_div = document.createElement('div');
-    flit_data_div.classList.add("flit-username");
-    flit_data_div.classList.add("flit-timestamp");
-
-    const username = document.createElement('a');
-    username.innerText = flitJSON.username;
-    username.href = `user/${flitJSON.userHandle}`;
-    username.classList.add("user-handle");
-
-    const handle = document.createElement('a');
-    handle.innerText = '@' + flitJSON.userHandle;
-    handle.href = `user/${flitJSON.userHandle}`;
-    handle.classList.add("user-handle");
-       // Add the online status to the flitJSON object
-    const onlineStatus = flitJSON.onlineStatus;
-    if (onlineStatus) {
-      // Add a green circle next to the user's handle
-      const greenCircle = document.createElement("span");
-      greenCircle.style.backgroundColor = "green";
-      greenCircle.style.borderRadius = "50%";
-      greenCircle.style.width = "10px";
-      greenCircle.style.height = "10px";
-      greenCircle.style.display = "inline-block";
-      greenCircle.style.marginLeft = "5px";
-      handle.parentNode.insertBefore(greenCircle, handle.nextSibling);
-    } else {
-      // The user is offline, remove the green circle if it exists
-      const nextSibling = handle.nextSibling;
-      if (nextSibling && nextSibling.nodeType === Node.ELEMENT_NODE && nextSibling.style.backgroundColor === "green") {
-        nextSibling.parentNode.removeChild(nextSibling);
-      }
-    }
-    let timestamp = new Date(flitJSON.timestamp.replace(/-/g, "/").replace(/T/, " "));
-    
-    // Format the Date object
-    let now = new Date();
-    let formatted_timestamp;
-
-    if (timestamp.getFullYear() !== now.getFullYear()) {
-      let options = { year: 'numeric', month: 'short', day: 'numeric' };
-      formatted_timestamp = timestamp.toLocaleDateString(undefined, options);
-    } else {
-      let monthAbbreviation = getMonthAbbreviation(timestamp);
-      formatted_timestamp = monthAbbreviation + " " + timestamp.getDate();
-    }
-
-    const timestampElement = document.createElement("span");
-    timestampElement.innerText = formatted_timestamp;
-    timestampElement.classList.add("user-handle");
-    
-    flit_data_div.appendChild(username);
-    flit_data_div.innerHTML += '&#160;&#160;';
-    flit_data_div.appendChild(handle);
-    flit_data_div.innerHTML += '&#160;·&#160;';
-    flit_data_div.appendChild(timestampElement);
-    flit_data_div.innerHTML += `<button style="float: right; border: none;" onclick='openReportModal(${flitJSON.id})' aria-label='report'><span class="iconify" data-icon="mdi:report" data-width="25"></span></button>`;
-
-    flit.appendChild(flit_data_div);
-
-    const flitContentDiv = document.createElement('div');
-    flitContentDiv.classList.add('flit-content');
-
-    if (flitJSON.is_reflit) {
-      const originalFlit = document.createElement('div');
-      originalFlit.classList.add('flit');
-      originalFlit.classList.add('originalFlit');
-      originalFlit.dataset.flitId = flitJSON.original_flit_id;
-      if (await renderSingleFlit(originalFlit) == 'profane') {
-        continue;
-      }
-      flitContentDiv.appendChild(document.createElement('br'));
-      flitContentDiv.appendChild(originalFlit);
-      flit.appendChild(flitContentDiv);
-    } else {
-      const content = document.createElement('a');
-      content.innerText = flitJSON.content + ' ' + flitJSON.hashtag;
-      content.href = `/flits/${flitJSON.id}`;
-
-      flitContentDiv.appendChild(content);
-      if (flitJSON.meme_link && (localStorage.getItem('renderGifs') == 'true' || localStorage.getItem('renderGifs') == undefined)) {
-        const image = document.createElement('img');
-        image.src = flitJSON.meme_link;
-        image.width = 200;
-        flitContentDiv.appendChild(document.createElement('br'));
-        flitContentDiv.appendChild(image);
-      }
-
-      flit.appendChild(flitContentDiv);
-    }
-
-    const reflitForm = document.createElement('form');
-    reflitForm.action = '/submit_flit';
-    reflitForm.method = 'POST';
-
-    const reflitInput = document.createElement('input');
-    reflitInput.type = 'hidden';
-    reflitInput.name = 'original_flit_id';
-    reflitInput.value = flitJSON.id;
-    reflitForm.appendChild(reflitInput);
-    reflitForm.innerHTML += '<button type="submit" class="retweet-button" aria-label="reflit"><span class="iconify" data-icon="ps:retweet-1"></span></button>';
-
-    flit.appendChild(reflitForm);
-
-    flit.innerHTML += '';
-    flit.href = `/flits/${flitJSON.id}`;
-
-    // Add flit in flits div
+    let flit = document.createElement("div");
+    flit.classList.add("flit");
+    flit = await renderFlitWithFlitJSON({"flit": flitJSON}, flit);
     flits.appendChild(flit);
   }
+  checkGreenDot();
   skip += limit;
 }
 renderFlits();
@@ -140,9 +33,14 @@ async function renderSingleFlit(flit) {
   if (await res.clone().text() == 'profane') {
     return 'profane';
   }
-  console.log(await res.clone().text());
   const json = await res.json();
-  console.log(json)
+  flit = renderFlitWithFlitJSON(json, flit);
+
+  flit.href = `/flits/${flitId}`;
+  checkGreenDot();
+}
+
+async function renderFlitWithFlitJSON(json, flit) {
   if (json['flit']) {
     const flit_data_div = document.createElement('div');
     flit_data_div.classList.add("flit-username");
@@ -181,7 +79,7 @@ async function renderSingleFlit(flit) {
     flit_data_div.appendChild(handle);
     flit_data_div.innerHTML += '&#160;·&#160;';
     flit_data_div.appendChild(timestampElement);
-    flit_data_div.innerHTML += `<button style="float: right; border: none;" onclick='openReportModal(${flitId})'><span class="iconify" data-icon="mdi:report" data-width="25"></span></button>`;
+    flit_data_div.innerHTML += `<button style="float: right; border: none;" onclick='openReportModal(${json.flit.id})'><span class="iconify" data-icon="mdi:report" data-width="25"></span></button>`;
 
     flit.appendChild(flit_data_div);
 
@@ -202,7 +100,7 @@ async function renderSingleFlit(flit) {
     } else {
       const content = document.createElement('a');
       content.innerText = json.flit.content + ' ' + json.flit.hashtag;
-      content.href = `/flits/${flitId}`;
+      content.href = `/flits/${json.flit.id}`;
 
       flitContentDiv.appendChild(content);
       if (json.flit.meme_link && (localStorage.getItem('renderGifs') == 'true' || localStorage.getItem('renderGifs') == undefined)) {
@@ -223,7 +121,7 @@ async function renderSingleFlit(flit) {
     const reflitInput = document.createElement('input');
     reflitInput.type = 'hidden';
     reflitInput.name = 'original_flit_id';
-    reflitInput.value = flitId;
+    reflitInput.value = json.flit.id;
     reflitForm.appendChild(reflitInput);
     reflitForm.innerHTML += '<button type="submit" class="retweet-button"><span class="iconify" data-icon="ps:retweet-1"></span></button>';
 
@@ -231,7 +129,7 @@ async function renderSingleFlit(flit) {
 
     flit.innerHTML += '';
   }
-  flit.href = `/flits/${flitId}`;
+  return flit;
 }
 
 const flitsList = document.getElementsByClassName('flit');
@@ -246,7 +144,53 @@ renderAll();
 
 window.onscroll = function (ev) {
   if (Math.round(window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-    console.log(skip);
     renderFlits();
   }
 };
+
+async function checkGreenDot() {
+  const res = await fetch("/api/render_online");
+  const data = await res.json();
+  // Get the list of online users
+  const onlineUsers = Object.keys(data);
+
+  // Update the user page
+  const handles = document.querySelectorAll(".user-handle");
+
+
+  handles.forEach((handle, index) => {
+    if (index % 3 != 0) {
+      return;
+    }
+    // Remove the green circle from the user
+    const nextSibling = handle.nextSibling;
+    if (nextSibling && nextSibling.nodeType === Node.ELEMENT_NODE && (nextSibling.style.backgroundColor === "green" || nextSibling.style.backgroundColor === "grey")) {
+      nextSibling.parentNode.removeChild(nextSibling);
+    }
+  
+    // Check if the user is online
+    if (onlineUsers.includes(handle.innerText)) {
+      // Add a green circle next to the user's handle
+      const greenCircle = document.createElement("span");
+      greenCircle.style.backgroundColor = "green";
+      greenCircle.style.borderRadius = "50%";
+      greenCircle.style.width = "10px";
+      greenCircle.style.height = "10px";
+      greenCircle.style.display = "inline-block";
+      greenCircle.style.marginLeft = "5px";
+      handle.parentNode.insertBefore(greenCircle, handle.nextSibling);
+    } else {
+      // Add a green circle next to the user's handle
+      const greyCircle = document.createElement("span");
+      greyCircle.style.backgroundColor = "grey";
+      greyCircle.style.borderRadius = "50%";
+      greyCircle.style.width = "10px";
+      greyCircle.style.height = "10px";
+      greyCircle.style.display = "inline-block";
+      greyCircle.style.marginLeft = "5px";
+      handle.parentNode.insertBefore(greyCircle, handle.nextSibling);
+    }
+  })
+}
+
+window.setInterval(checkGreenDot, 5000);
