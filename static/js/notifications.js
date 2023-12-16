0adde382139ad56bf045d91ec1fbe735c77875e1
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2023-12-16 01:11:11 +0000

    notifications!

---
+++ static/js/notifications.js
@@ -0,0 +1,29 @@
+console.log('notifications.js Loaded')
+const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+let prevRecentMessages;
+async function checkNotifications() {
+  const res = await fetch(`/api/get_flits?skip=0&limit=${limit}`);
+  if (!equals(await res.clone().json(), prevRecentMessages)) {
+    prevRecentMessages = await res.json();
+    if (Notification.permission == 'granted') {
+      console.log("Notification");
+      const notification = new Notification('Message in Main', {
+        icon: '/static/logo.png',
+        body: 'Somebody sent something on tweetor'
+      })
+    }
+  }
+}
+
+Notification.requestPermission();
+if (Notification.permission !== 'denied') {
+  Notification.requestPermission();
+}
+
+(async () => {
+  const res = await fetch(`/api/get_flits?skip=${skip}&limit=${limit}`);
+  console.log(await res.clone().json());
+  prevRecentMessages = await res.json();
+})();
+
+window.setInterval(checkNotifications, 1000);
