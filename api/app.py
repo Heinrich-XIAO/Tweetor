4b8ff5021335506144acb7826409ef3a4173330a
Heinrich-XIAO <heinrichxiao@gmail.com>
2024-01-06 10:15:28 -0500

    filesystem

--- api/app.py
+++ api/app.py
@@ -70,7 +70,7 @@ limiter = Limiter(get_remote_address, app=app)
 
 # Set up the session object
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "redis"
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 staff_accounts = ["ItsMe", "Dude_Pog"]
