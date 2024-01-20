751d4cdfb4203b76d24e5bd163e39b9c86173ce9
Heinrich-XIAO <heinrichxiao@gmail.com>
2024-01-20 18:33:04 -0500

    notifications with multiple flits

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -4,17 +4,26 @@ let prevRecentMessages;
 const notificationAudio = document.getElementById("notification");
 console.log(notificationAudio);
 
+function findAddedElements(oldArray, newArray) {
+    const addedElements = newArray.filter(newObj => !oldArray.some(oldObj => oldObj["id"] === newObj["id"]));
+    return addedElements;
+}
+
 async function checkNotifications() {
   const res = await fetch(`/api/get_flits?skip=0&limit=${limit}`);
-  if (!equals(await res.clone().json(), prevRecentMessages)) {
+  const newElements = findAddedElements(prevRecentMessages, await res.clone().json());
+  console.log(newElements);
+  if (newElements.length != 0) {
     if (window.location.pathname == '/') {
-      console.log((await res.clone().json())[0]);
-      let flit = document.createElement("div");
-      flit.classList.add("flit");
-      flit.dataset.flitId = (await res.clone().json())[0]['id'];
-      flit = await renderSingleFlit(flit);
-      console.log(flit);
-      flits.insertBefore(flit, flits.firstChild);
+      for (let i = 0; i < newElements.length; i++) {
+        console.log((await res.clone().json())[i]);
+        let flit = document.createElement("div");
+        flit.classList.add("flit");
+        flit.dataset.flitId = (await res.clone().json())[i]['id'];
+        flit = await renderSingleFlit(flit);
+        console.log(flit);
+        flits.insertBefore(flit, flits.firstChild);
+      }    
     }
     prevRecentMessages = await res.json();
     if (Notification.permission == 'granted') {
@@ -39,7 +48,7 @@ if (Notification.permission !== 'denied') {
 
   res = await fetch(`/api/get_handle`);
   if (localStorage.getItem('notifications') == 'true' || localStorage.getItem('notifications') == undefined && (await res.text()) != 'Not Logged In') {
-    window.setInterval(checkNotifications, 1000);
+    window.setInterval(checkNotifications, 5000);
   }
 })();
 
