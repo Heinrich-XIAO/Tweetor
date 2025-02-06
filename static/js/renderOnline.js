48c65d0bb32889aba43d828c286202e3178bc773
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-02-06 21:19:18 +0000

    fix: doesn't cache /api/renderOnline requests

--- static/js/renderOnline.js
+++ static/js/renderOnline.js
@@ -1,7 +1,8 @@
 console.log("renderOnline.js loaded");
 
-fetch("/api/render_online");
+fetch("/api/render_online", { cache: "no-store" });
 
-window.setInterval(()=> {
-  fetch("/api/render_online");
-}, 10000)
+window.setInterval(() => {
+  fetch("/api/render_online", { cache: "no-store" });
+}, 10000);
+
