14f50a5a29f1e90e8574a1c0161b27d5905adbf0
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-03-16 00:14:01 -0400

    get rid of dead function

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,9 +1,3 @@
 const socket = io({
   transports: ['websocket']
 });
-
-function showNotification(message) {
-  new Notification(`${message.sender_handle + ' '}said ...`, {
-    body: `${message.content}`,
-  });
-}
